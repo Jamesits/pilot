74eb712329f0f6bd55469a95a92f4aba33832fbe
James Swineson <github@public.swineson.me>
2020-04-04 21:38:39 +0800

    fix flake8 and pytest

--- pilot/gobgp_web/gobgp_connector.py
+++ pilot/gobgp_web/gobgp_connector.py
@@ -1,23 +1,18 @@
-import sys
-
-from flask import current_app as app
-
-sys.path.append('./gobgp_interface')
-
 import grpc
+from flask import current_app as app
+from protobuf_to_dict import protobuf_to_dict
 
 from pilot.gobgp_interface import gobgp_pb2
 from pilot.gobgp_interface import gobgp_pb2_grpc
 
-from protobuf_to_dict import protobuf_to_dict
 
 def get_peers():
     channel = grpc.insecure_channel(f"{app.config['GOBGP_IP']}:{app.config['GOBGP_PORT']}")
     stub = gobgp_pb2_grpc.GobgpApiStub(channel)
-    
+
     peers = stub.ListPeer(gobgp_pb2.ListPeerRequest(), app.config['GOBGP_TIMEOUT_MS'])
     ret = []
-    
+
     for peer in peers:
         peer = peer.peer
         print("BGP neighbor is %s, remote AS %d" % (peer.conf.neighbor_address, peer.conf.peer_as))
@@ -27,6 +22,7 @@ def get_peers():
         ret.append(protobuf_to_dict(peer))
 
     return ret
+
 
 def get_table():
     channel = grpc.insecure_channel(f"{app.config['GOBGP_IP']}:{app.config['GOBGP_PORT']}")
@@ -40,22 +36,22 @@ def get_table():
     #     # name=,
     # ), app.config['GOBGP_TIMEOUT_MS'])
 
+
 def get_routes():
     channel = grpc.insecure_channel(f"{app.config['GOBGP_IP']}:{app.config['GOBGP_PORT']}")
     stub = gobgp_pb2_grpc.GobgpApiStub(channel)
 
     routes = stub.ListPath(gobgp_pb2.ListPathRequest(
         table_type=gobgp_pb2.GLOBAL,
-        #name="",
+        # name="",
         family=gobgp_pb2.Family(
-            afi =  gobgp_pb2._FAMILY_AFI.values_by_name['AFI_IP'].number,
-            safi = gobgp_pb2._FAMILY_SAFI.values_by_name["SAFI_FLOW_SPEC_UNICAST"].number,
+            afi=gobgp_pb2._FAMILY_AFI.values_by_name['AFI_IP'].number,
+            safi=gobgp_pb2._FAMILY_SAFI.values_by_name["SAFI_FLOW_SPEC_UNICAST"].number,
         ),
     ), app.config['GOBGP_TIMEOUT_MS'])
 
     ret = []
     for route in routes:
         ret.append(protobuf_to_dict(route))
-    
-    return ret
 
+    return ret

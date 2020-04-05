38d25d3d99b5f49f34c179c4f376d6af55abb17f
James Swineson <github@public.swineson.me>
2020-04-05 17:50:39 +0800

    open source under AGPLv3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='pilot',
     version='0.0.1',
     description='BGP flowspec based SDN controller at home',
-    license="Proprietary",
+    license="AGPLv3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='James Swineson',
@@ -23,6 +23,11 @@ setup(
     url="https://github.com/Jamesits/Pilot",
     packages=find_packages(),
     install_requires=install_reqs,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
     entry_points={
         'console_scripts': [
             'pilot_server=pilot.__main__:main'
